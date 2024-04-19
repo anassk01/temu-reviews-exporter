1002af8c6b039af64ed8046bb81f0cafa7ef9071
anassk01 <anaskabil01@gmail.com>
2024-04-19 19:44:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,48 @@
 class ReviewExtractor {
     constructor() {
         this.extractedInfo = [];
-        this.reviews = document.querySelectorAll('#reviewContent > div._3mn9Y0tf > div._2OaJDN8Y._3Rsl6Owq._1xAk_zzX > div._10EiyDKr._3hjIP4Y2._2gC1sYKf.HVCrPqZh > div > div > div > div:nth-child(3) > div > div > div');
     }
 
-    extractReviewInfo() {
-        this.reviews.forEach((review) => {
-            const authorNameElement = review.querySelector('.XTEkYdlM._3a8V1xkt');
-            const authorName = authorNameElement ? authorNameElement.textContent : '';
-            
-            const authorImageElement = review.querySelector('.M-mQ_cI0 img');
-            const authorImage = authorImageElement ? authorImageElement.src : '';
-            
-            const countryImageElement = review.querySelector('div.jr_zeQBk._2RiUeNfG > span:nth-child(2) > img');
-            const countryImage = countryImageElement ? countryImageElement.src : '';
+extractReviewInfo() {
+    const reviews = document.querySelectorAll('#reviewContent > div._3mn9Y0tf > div._2OaJDN8Y._3Rsl6Owq._1xAk_zzX > div._10EiyDKr._3hjIP4Y2._2gC1sYKf.HVCrPqZh > div > div > div > div:nth-child(3) > div > div > div');
+    
+    reviews.forEach((review) => {
+        const authorNameElement = review.querySelector('.XTEkYdlM._3a8V1xkt');
+        const authorName = authorNameElement ? authorNameElement.textContent : '';
+        
+        const authorImageElement = review.querySelector('.M-mQ_cI0 img');
+        const authorImage = authorImageElement ? authorImageElement.src : '';
+        
+        const countryImageElement = review.querySelector('div.jr_zeQBk._2RiUeNfG > span:nth-child(2) > img');
+        const countryImage = countryImageElement ? countryImageElement.src : '';
 
-            const reviewDateElement = review.querySelector('div.jr_zeQBk._2RiUeNfG > span:nth-child(3)');
-            const reviewDate = reviewDateElement ? reviewDateElement.innerText : '';
+        const reviewDateElement = review.querySelector('div.jr_zeQBk._2RiUeNfG > span:nth-child(3)');
+        const reviewDate = reviewDateElement ? reviewDateElement.innerText : '';
 
-            const starIcons = review.querySelectorAll('.ZJF4Fxlw svg');
-            const numOfStars = starIcons.length;
-            const starEmoji = '⭐'.repeat(numOfStars);
+        const starIcons = review.querySelectorAll('.ZJF4Fxlw svg');
+        const numOfStars = starIcons.length;
+        const starEmoji = '⭐'.repeat(numOfStars);
 
-            const bodyImages = review.querySelectorAll('.splide__list img');
-            const imageUrls = Array.from(bodyImages).map(img => img.src);
+        const bodyImages = review.querySelectorAll('.splide__list img');
+        const imageUrls = Array.from(bodyImages).map(img => img.src);
 
-            const bodyTextElement = review.querySelector('span._2EO0yd2j');
-            const bodyText = bodyTextElement ? bodyTextElement.textContent : '';
+        const bodyTextElement = review.querySelector('span._2EO0yd2j');
+        const bodyText = bodyTextElement ? bodyTextElement.textContent : '';
 
-            this.extractedInfo.push({
-                'Author Name': authorName,
-                'Author Image': authorImage,
-                'Country Image': countryImage,
-                'Review Date': reviewDate,
-                'Stars': starEmoji,
-                'Images': imageUrls.join(';'), // Join image URLs as a string
-                'Body Text': bodyText
-            });
+        this.extractedInfo.push({
+            'Author Name': authorName,
+            'Author Image': authorImage,
+            'Country Image': countryImage,
+            'Review Date': reviewDate,
+            'Stars': starEmoji,
+            'Images': imageUrls.join(';'), // Join image URLs as a string
+            'Body Text': bodyText
         });
-    }
+    });
+}
+
 
     convertJsonToCsv() {
-        this.extractReviewInfo(); // Extract review info before converting to CSV
         const csvContent = "data:text/csv;charset=utf-8," 
             + "Author Name,Author Image,Country Image,Review Date,Stars,Images,Body Text\n"
             + this.extractedInfo.map(item => {
@@ -52,6 +53,7 @@ class ReviewExtractor {
     }
 
     downloadCsv() {
+        this.extractReviewInfo();
         const csvData = this.convertJsonToCsv();
         const link = document.createElement('a');
         link.setAttribute('href', csvData);
@@ -61,11 +63,12 @@ class ReviewExtractor {
     }
 
     convertJsonToJsonString() {
-        this.extractReviewInfo(); // Extract review info before converting to JSON
+        this.extractReviewInfo();
         return JSON.stringify(this.extractedInfo);
     }
 
     downloadJson() {
+        this.extractReviewInfo();
         const jsonData = this.convertJsonToJsonString();
         const blob = new Blob([jsonData], { type: 'application/json' });
         const url = URL.createObjectURL(blob);
@@ -77,17 +80,20 @@ class ReviewExtractor {
     }
 }
 
-// Create an instance of ReviewExtractor
-const reviewExtractor = new ReviewExtractor();
-
-// Dynamically create buttons and add them to the DOM after the target element
+// Dynamically create buttons and add them to the DOM
 const csvButton = document.createElement('button');
 csvButton.textContent = 'Download CSV';
-csvButton.addEventListener('click', () => reviewExtractor.downloadCsv());
+csvButton.addEventListener('click', () => {
+    const reviewExtractor = new ReviewExtractor();
+    reviewExtractor.downloadCsv();
+});
 
 const jsonButton = document.createElement('button');
 jsonButton.textContent = 'Download JSON';
-jsonButton.addEventListener('click', () => reviewExtractor.downloadJson());
+jsonButton.addEventListener('click', () => {
+    const reviewExtractor = new ReviewExtractor();
+    reviewExtractor.downloadJson();
+});
 
 const targetDiv = document.querySelector('div._39vL3TE4');
 targetDiv.insertAdjacentElement('afterend', document.createElement('br'));
